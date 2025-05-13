e9d81fc005a237ba7e244b6284c9eb4ea5c5a3aa
kcsanju444 <kcsanju600@gmaill.com>
2025-05-13 16:15:14 +0545

    updated

--- client/src/Components/Start.jsx
+++ client/src/Components/Start.jsx
@@ -20,7 +20,7 @@ const Start = () => {
   }, [])
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-blue loginPage">
       <div className="p-3 rounded w-25 border loginForm">
         <h2 className="text-center">Login As</h2>
         <div className="d-flex justify-content-between mt-5 mb-2">
