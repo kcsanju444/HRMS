ed726ccad3c219dda45d90e946f79acbeeda03d2
kcsanju444 <kcsanju600@gmaill.com>
2025-05-13 17:35:58 +0545

    install tailwind and work on the frontend

--- client/src/Components/Start.jsx
+++ client/src/Components/Start.jsx
@@ -1,33 +1,26 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-useEffect
 
 const Start = () => {
-    const navigate = useNavigate()
-  axios.defaults.withCredentials = true;
-  useEffect(() => {
-    axios.get('http://localhost:3000/verify')
-    .then(result => {
-      if(result.data.Status) {
-        if(result.data.role === "admin") {
-          navigate('/dashboard')
-        } else {
-          navigate('/employee_detail/'+result.data.id)
-        }
-      }
-    }).catch(err =>console.log(err))
-  }, [])
+  const navigate = useNavigate();
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-blue loginPage">
-      <div className="p-3 rounded w-25 border loginForm">
-        <h2 className="text-center">Login As</h2>
-        <div className="d-flex justify-content-between mt-5 mb-2">
-          <button type="button" className="btn btn-primary" onClick={() => {navigate('/employee_login')}}>
+    <div className="flex justify-center items-center min-h-screen bg-blue-500">
+      <div className="p-5 rounded-lg w-1/3 bg-white shadow-lg">
+        <h2 className="text-center text-xl font-semibold mb-6">Login As</h2>
+        <div className="flex justify-between mb-4">
+          <button
+            type="button"
+            className="w-full py-3 text-white bg-blue-500 hover:bg-blue-600 rounded-md mr-2"
+            onClick={() => navigate("/employee_login")}
+          >
             Employee
           </button>
-          <button type="button" className="btn btn-success" onClick={() => {navigate('/adminlogin')}}>
+          <button
+            type="button"
+            className="w-full py-3 text-white bg-green-500 hover:bg-green-600 rounded-md ml-2"
+            onClick={() => navigate("/adminlogin")}
+          >
             Admin
           </button>
         </div>
